1e70817446ca5fbc8cc68839d4a91f3f91fbbb89
RaidWLf <ambuj.kumar.7903@gmail.com>
2023-05-11 22:11:43 +0530

    removed bug of nodemon

--- index.js
+++ index.js
@@ -7,3 +7,5 @@ const port = process.env.PORT || 5000;
 app.use(express.urlencoded({ extended: true }));
 
 app.post("/", (req, res) => console.log(req.body));
+
+app.listen(port, (req, res) => console.log(`running on ${port}`));
