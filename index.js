587e3ccbaceaf96074eea6e0a8e4e995f1a84b8a
RaidWLf <ambuj.kumar.7903@gmail.com>
2023-05-11 22:04:31 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require("express");
+
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+app.use(express.urlencoded({ extended: true }));
+
+app.post("/", (req, res) => console.log(req.body));
