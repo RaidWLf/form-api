d6cbb3acc5c191b361e50e88b2f925cdcd2220ee
RaidWLf <ambuj.kumar.7903@gmail.com>
2023-05-12 13:44:28 +0530

    removed bug of env

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ const port = process.env.PORT || 5000;
 
 app.use(express.urlencoded({ extended: true }));
 
-app.post("/", (req, res) => console.log(req.body));
+app.post("/", (req, res) => res.status(200).json(req.body));
 
 app.listen(port, (req, res) => console.log(`running on ${port}`));
