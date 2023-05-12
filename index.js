48ce9125dab2ddc18798b0bd0b1c17cfbb0ed03e
RaidWLf <ambuj.kumar.7903@gmail.com>
2023-05-12 13:50:41 +0530

    added Home Page

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const port = process.env.PORT || 5000;
 
 app.use(express.urlencoded({ extended: true }));
 
-app.post("/", (req, res) => res.status(200).json(req.body));
+app.get("/", (req, res) => res.send("Hello to Form API"));
+
+app.post("/api", (req, res) => res.status(200).json(req.body));
 
 app.listen(port, (req, res) => console.log(`running on ${port}`));
